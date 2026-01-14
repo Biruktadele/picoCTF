0fdcdfd299a75726216d7e78970840a31a86e53a
biruktadele <biruktadele55@gmail.com>
2026-01-14 20:14:02 +0300

    c3 convert.py

---
+++ WEB/Cryptography/c3/convert.py
@@ -0,0 +1,25 @@
+import sys
+chars = ""
+s = "DLSeGAGDgBNJDQJDCFSFnRBIDjgHoDFCFtHDgJpiHtGDmMAQFnRBJKkBAsTMrsPSDDnEFCFtIbEDtDCIbFCFtHTJDKerFldbFObFCFtLBFkBAAAPFnRBJGEkerFlcPgKkImHnIlATJDKbTbFOkdNnsgbnJRMFnRBNAFkBAAAbrcbTKAkOgFpOgFpOpkBAAAAAAAiClFGIPFnRBaKliCgClFGtIBAAAAAAAOgGEkImHnIl"
+
+for line in s:
+  chars += line
+
+lookup1 = "\n \"#()*+/1:=[]abcdefghijklmnopqrstuvwxyz"
+lookup2 = "ABCDEFGHIJKLMNOPQRSTabcdefghijklmnopqrst"
+
+out = ""
+
+prev = 0
+for char in chars:
+  cur = lookup1.index(char)
+  out += lookup2[(cur - prev) % 40]
+  prev = cur
+
+sys.stdout.write(out)
+s = ""
+for i in range(len(s)-1,1):
+    pre = lookup2.index(s[i-1])
+    ot = lookup2.index(s[i])
+
+    
